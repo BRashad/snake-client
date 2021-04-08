237244cca009133576158aed1d8d5a303f4e1c0a
Rashad Bayram <rashadabayram.com>
2021-04-07 18:04:50 -0600

    user inpunt handler added

--- play.js
+++ play.js
@@ -5,3 +5,23 @@
 const { connect } = require('./client.js');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.handleUserInput('data', (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+}
+
+setupInput();
